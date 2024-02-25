167f3e980762ebb6c9ff51915f507dc52d5f44a1
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2024-02-24 22:44:31 -0600

    Primeros pasos con Jest

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // const {getAge,uuidv4} = require('./plugins')
 
-import { getPokemonById } from "./js-foundation/06-promises";
-import { buildLogger } from "./plugins/logger.plugin";
+// import { getPokemonById } from "./js-foundation/06-promises";
+// import { buildLogger } from "./plugins/logger.plugin";
 
 // const {emailTemplate} = require('./js-foundation/01-template')
 // require('./js-foundation/02-destructuring')
@@ -10,19 +10,19 @@ import { buildLogger } from "./plugins/logger.plugin";
 // const {makeBuildPerson} = require('./js-foundation/05-factory')
 // const getPokemonById = require('./js-foundation/06-promises')
 // const {buildLogger} = require('./plugins')
-const logger = buildLogger('app.js')
-logger.log('Hola mundo')
-logger.error('Este es un error')
+// const logger = buildLogger('app.js')
+// logger.log('Hola mundo')
+// logger.error('Este es un error')
 
 // getPokemonById(5,(pokemon)=>{
 //     console.log(pokemon)
 // })
 
 
-getPokemonById(6)
-    .then((pokemon) => console.log({pokemon}))
-    .catch((err) => console.log('No se encontro el pokemon'))
-    .finally(() => console.log('Finalmente'))
+// getPokemonById(6)
+//     .then((pokemon) => console.log({pokemon}))
+//     .catch((err) => console.log('No se encontro el pokemon'))
+//     .finally(() => console.log('Finalmente'))
 
 
 
