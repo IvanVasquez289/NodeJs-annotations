0447ccb54d835fcf309b1fde03d5734af2786a4b
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2024-02-25 12:35:26 -0600

    primer test

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,17 @@
-describe('App', () => {
-    it('should be true', () => {
-        expect(true).toBe(true)
+
+// A A A 
+
+describe('Test in the app file', () => {
+    it('should be 30', () => {
+        // 1. Arrange
+        const num1 = 20;
+        const num2 = 10;
+
+        // 2. Act
+        const result = num1 + num2;
+
+        // 3. Assert
+        expect(result).toBe(35)
+
     })
 })
